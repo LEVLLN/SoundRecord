702bcb3bf2b1c99b2ae808e215cdd6128d66377c
LEVLLN <lev.kudryashov1994@gmail.com>
2016-10-10 17:52:43 +0700

    10.10.16 registration package deleted Reservation class corrected +enum field +durationField Shedule class corrected +enum day of week

--- reservation/models.py
+++ reservation/models.py
@@ -10,7 +10,17 @@ class Schedule(models.Model):
     soundman = models.ForeignKey(User)
     start_of_the_day = models.TimeField()
     end_of_the_day = models.TimeField()
-    working_day = models.IntegerField()
+    DAY_OF_WEEK =(
+        (1,"Monday"),
+        (2,"Tuesday"),
+        (3, "Wednesday"),
+        (4,"Thursday"),
+        (5, "Friday"),
+        (6,"Saturday"),
+        (7,"Sunday")
+
+    )
+    working_day = models.IntegerField(choices=DAY_OF_WEEK)
 
 
 class Reservation(models.Model):
