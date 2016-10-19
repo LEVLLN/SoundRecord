b6870366a3c1134e528b530c6aaaac0ab52a7953
TRPO6 <lln-nz@mail.ru>
2016-10-19 20:55:58 +0700

    added slashs

--- reservation/urls.py
+++ reservation/urls.py
@@ -3,7 +3,7 @@ from reservation import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^register', views.Registration.register, name='register'),
-    url(r'^login', views.Authorization.login, name='login'),
-    url(r'^logout', views.Authorization.logout, name='logout')
+    url(r'^register/', views.Registration.register, name='register'),
+    url(r'^login/', views.Authorization.login, name='login'),
+    url(r'^logout/', views.Authorization.logout, name='logout')
 ]
